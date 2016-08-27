1718debd3fef345ca97d66fbc09fea6f69bde09a
mikeXwang <mikeXwang@163.com>
2016-08-27 13:27:56 +0800

    add some comment for NDK

--- app/src/main/java/com/mikexapp/mikex/mikexapp/JNITest.java
+++ app/src/main/java/com/mikexapp/mikex/mikexapp/JNITest.java
@@ -4,6 +4,18 @@ package com.mikexapp.mikex.mikexapp;
  * Created by mike on 16-8-27.
  */
 
+
+/**
+ * This is left for NDK interface demo
+ * Ndk configuration is as follows:
+ * 1.add a java file like this file.It include the interface with native announcement which you need
+ * 2.run javah to generate .h file
+ * 3.add config in build.gradle
+ * 4.loadlibrary
+ * 5.use the interface
+ *
+ *
+ */
 public class JNITest {
 
 
