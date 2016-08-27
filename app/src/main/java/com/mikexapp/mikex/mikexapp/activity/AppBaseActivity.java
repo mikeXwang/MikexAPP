b86865ab25075f01a4f6a98597ab664245bc0425
mikeXwang <mikeXwang@163.com>
2016-08-27 14:07:33 +0800

    1.make an app frame 2.add some base class to be implemented.

---
+++ app/src/main/java/com/mikexapp/mikex/mikexapp/activity/AppBaseActivity.java
@@ -0,0 +1,15 @@
+package com.mikexapp.mikex.mikexapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.mikexapp.mikex.mikexapp.R;
+
+public class AppBaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_base);
+    }
+}
