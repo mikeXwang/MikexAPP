9bd75ccf89607b74b26b5be7f8df4c72daf999a3
mikeXwang <mikeXwang@163.com>
2016-08-28 10:13:15 +0800

    modify some tags

--- app/src/main/java/com/mikexapp/mikex/mikexapp/fragment/AppBaseFragment.java
+++ app/src/main/java/com/mikexapp/mikex/mikexapp/fragment/AppBaseFragment.java
@@ -1,10 +1,13 @@
 package com.mikexapp.mikex.mikexapp.fragment;
 
 import android.app.Fragment;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 /**
  * Created by mike on 16-8-28.
  */
 
+@RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
 public class AppBaseFragment extends Fragment{
 }
