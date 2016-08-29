2db13baabf73351352f631d34e746b89ca23c4d9
mikeXwang <mikeXwang@163.com>
2016-08-30 00:41:23 +0800

    1.add lib module which used for the future to build an operating environment for apps

---
+++ lib/src/main/java/com/mikexapp/mikex/lib/TestClass.java
@@ -0,0 +1,13 @@
+package com.mikexapp.mikex.lib;
+
+/**
+ * Created by mike on 16-8-30.
+ */
+
+/**
+ * This lib is used for build an app operating environment,including hook .etc.
+ * Now this class is just used for commiting code.
+ *
+ */
+public class TestClass {
+}
