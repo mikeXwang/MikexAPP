d09810e98a1c2fa5a25fbdff30f4bb04c019b6c8
mikeXwang <mikeXwang@163.com>
2016-09-02 00:59:24 +0800

    1.add scan apks in sdcard 2.add code to hook ActivityManageService
    !!!The code must be refactored quickly!!!
    
    Now if we dont do it,the project will be totally distorted.
    
    We must finish refactoring this weekend.

--- app/src/main/java/com/mikexapp/mikex/mikexapp/adapter/StoreCardAppAdapter.java
+++ app/src/main/java/com/mikexapp/mikex/mikexapp/adapter/StoreCardAppAdapter.java
@@ -11,6 +11,8 @@ import android.widget.TextView;
 import com.mikexapp.mikex.mikexapp.R;
 import com.mikexapp.mikex.mikexapp.model.AppInfo;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Proxy;
 import java.util.List;
 
 /**
@@ -44,12 +46,21 @@ public class StoreCardAppAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder viewHolder;
         View view = convertView;
 
         if(view == null) {
             view = mLayoutInflater.inflate(R.layout.item_store_card_app, null);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //start uninstalled apps in sdcard.
+                    AppInfo appinfo = (AppInfo) getItem(position);
+                    startAppByPackageName(appinfo.getPkgName());
+
+                }
+            });
             viewHolder = new ViewHolder();
             viewHolder.mImageViewHolder = (ImageView) view.findViewById(R.id.iv_item_store_card_app);
             viewHolder.mTextViewHolder = (TextView) view.findViewById(R.id.tv_item_store_card_app);
@@ -65,9 +76,42 @@ public class StoreCardAppAdapter extends BaseAdapter {
         return view;
     }
 
+    private void startAppByPackageName(String pkgName) {
+
+        //hook ActivityManagerService
+        try {
+            Class<?> activityManagerNativeClass = Class.forName("android.app.ActivityManagerNative");
+            Field gDefaultField = activityManagerNativeClass.getDeclaredField("gDefault");
+            gDefaultField.setAccessible(true);
+            Object gDefault = gDefaultField.get(null);
+
+            Class<?> singleton = Class.forName("android.util.Singleton");
+            Field mInstanceField = singleton.getDeclaredField("mInstance");
+            mInstanceField.setAccessible(true);
+
+            // ActivityManagerNative 的gDefault对象里面原始的 IActivityManager对象
+            Object rawIActivityManager = mInstanceField.get(gDefault);
+            // 创建一个这个对象的代理对象, 然后替换这个字段, 让我们的代理对象帮忙干活
+            Class<?> iActivityManagerInterface = Class.forName("android.app.IActivityManager");
+//            Object proxy = Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+//                    new Class<?>[] { iActivityManagerInterface }, new IActivityManagerHandler(rawIActivityManager));
+//            mInstanceField.set(gDefault, proxy);
+
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     class ViewHolder {
         protected ImageView mImageViewHolder;
         protected TextView mTextViewHolder;
     }
+
+
 }
 
--- app/src/main/java/com/mikexapp/mikex/mikexapp/fragment/StoreCardApkFragment.java
+++ app/src/main/java/com/mikexapp/mikex/mikexapp/fragment/StoreCardApkFragment.java
@@ -1,6 +1,11 @@
 package com.mikexapp.mikex.mikexapp.fragment;
 
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -16,6 +21,7 @@ import com.mikexapp.mikex.mikexapp.adapter.InstalledAppAdapter;
 import com.mikexapp.mikex.mikexapp.adapter.StoreCardAppAdapter;
 import com.mikexapp.mikex.mikexapp.model.AppInfo;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,9 +29,10 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class StoreCardApkFragment extends AppBaseFragment {
-    private List<AppInfo> mStoreCardApps;
+    private ArrayList<AppInfo> mStoreCardApps;
     private ListView mListView;
     private StoreCardAppAdapter mStoreCardAppAdapter;
+
     public StoreCardApkFragment() {
         // Required empty public constructor
     }
@@ -38,7 +45,8 @@ public class StoreCardApkFragment extends AppBaseFragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_store_card_app, container, false);
-        mStoreCardApps = getTitleDataList();
+        mStoreCardApps = new ArrayList<AppInfo>();
+        doSearch(new File("/sdcard/apks"));
         mListView = (ListView) view.findViewById(R.id.list_view_store_card_app);
         mStoreCardAppAdapter = new StoreCardAppAdapter(getActivity(), mStoreCardApps);
         mListView.setAdapter(mStoreCardAppAdapter);
@@ -49,20 +57,52 @@ public class StoreCardApkFragment extends AppBaseFragment {
 
     /**
      * Notes:此处方法不应该放在主线程中，会造成页面卡顿，这里仅仅作调试使用
-     *
+     * <p>
      * This function should not add in main thread.
      * We add it here just for testing!!!!
+     *
      * @return
      */
     @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-    private List<AppInfo> getTitleDataList() {
-        List<AppInfo> listItem = new ArrayList<AppInfo>();
-        for(int i = 0; i < 20; i++) {
-            AppInfo data = new AppInfo(getActivity().getResources().getDrawable(R.drawable.abc_btn_check_material),"Application in SD card  " + i, null,null);
-            Log.d("www","data :"+data.getPkgName());
-            listItem.add(data);
+    private void doSearch(File file) {
+
+        if (!file.exists()) {
+            return;
         }
-        return listItem;
+        File[] files = file.listFiles();
+        if (files != null && files.length != 0) {
+            for (File f : files) {
+                if (f.isFile()) {
+                    if (!f.toString().endsWith(".apk")) {
+                        return;
+                    }
+                    AppInfo appinfo = new AppInfo();
+                    appinfo.setAppIcon(getApkIcon(getActivity(),f.getAbsolutePath()));
+                    appinfo.setPkgName(f.getAbsolutePath());
+                    mStoreCardApps.add(appinfo);
+                } else if (f.isDirectory()) {
+                    doSearch(f);
+                }
+            }
+        }
+
+    }
+
+    public static Drawable getApkIcon(Context context, String apkPath) {
+        PackageManager pm = context.getPackageManager();
+        PackageInfo info = pm.getPackageArchiveInfo(apkPath,
+                PackageManager.GET_ACTIVITIES);
+        if (info != null) {
+            ApplicationInfo appInfo = info.applicationInfo;
+            appInfo.sourceDir = apkPath;
+            appInfo.publicSourceDir = apkPath;
+            try {
+                return appInfo.loadIcon(pm);
+            } catch (OutOfMemoryError e) {
+                Log.e("ApkIconLoader", e.toString());
+            }
+        }
+        return null;
     }
 
 }
